31c4f1006af1b8ade1cbe3108577dc26d7f3a211
Emam999 <mdemamshikder1@gmail.com>
2021-10-02 22:59:35 -0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/emam/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.emam
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar (
+        backgroundColor: Colors.teal,
+        centerTitle: true,
+        title: Text("Batch C"),
+      ),
+      body: Container(
+        color: Colors.cyan,
+      ),
+    );
+  }
+}
